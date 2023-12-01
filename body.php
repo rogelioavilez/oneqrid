113e655cd3d9e7371323f46ead4dc95642688c8e
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-30 21:50:35 -0600

    test8

--- body.php
+++ body.php
@@ -2,16 +2,4 @@
 
     <?php include ("includes/jumbotron.php");?>
     
-    <section>
-        <article>
-            <h1>Article 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi veritatis quibusdam deserunt atque unde voluptatum dolorum suscipit amet consequatur, voluptates aut. Dolorum in corporis aspernatur voluptate voluptas est facilis.</p>
-        </article>
-
-        <article>
-            <h1>Article 2</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis reiciendis sed aut maiores ullam. Vel corporis illo alias provident aspernatur, dolores reiciendis delectus placeat? Dolorum unde itaque repudiandae illo!</p>
-        </article>
-    </section>
-    
 </body>
